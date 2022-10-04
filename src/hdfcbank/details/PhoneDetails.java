39022ba23cd4c6c66a53503b61018eac283b4836
GunasekaranE <gunasekaran270593@gmail.com>
2022-10-04 17:28:03 +0530

    BANK

---
+++ src/hdfcbank/details/PhoneDetails.java
@@ -0,0 +1,11 @@
+package hdfcbank.details;
+
+public class PhoneDetails {
+	private void storage() {
+System.out.println("128GB");
+}
+public static void main(String[] args) {
+	PhoneDetails a=new PhoneDetails();
+	a.storage();
+}
+}
